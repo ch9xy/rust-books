219849b343463dc566df24d09dcf6ac1ae61f0b6
ch9xy <67190054+ch9xy@users.noreply.github.com>
2023-08-18 14:08:26 +0300

    Add files via upload

---
+++ tuto1/fib.rs
@@ -0,0 +1,24 @@
+trait Float {
+    const ZERO: Self,
+    const ONE: Self,
+}
+
+
+impl Float for f32 {
+    const ZERO: f32 = 0.0;
+    const ONE: f32 = 1.0;
+}
+
+impl Float for f64 {
+    const ZERO: f64 = 0.0;
+    const ONE: f64 = 1.0;
+}
+
+
+fn fib<T: Float + Add<Output=T>>(n: usize) -> T {
+    match n {
+        0 => T::ZERO,
+        1 => T::ONE,
+        n => fib::<T>(n - 1) + fib::<T>(n - 2)
+    }
+}
