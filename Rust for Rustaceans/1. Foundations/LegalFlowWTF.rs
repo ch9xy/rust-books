10a9f689be34ccec76352e5d2283ef8334d5b2de
ch9xy <67190054+ch9xy@users.noreply.github.com>
2023-09-09 03:06:15 +0300

    Update LegalFlowWTF.rs

--- Rust for Rustaceans/1. Foundations/LegalFlowWTF.rs
+++ Rust for Rustaceans/1. Foundations/LegalFlowWTF.rs
@@ -2,12 +2,12 @@ fn main () {
     let mut x = Box::new(42);
     let r = &x;     // 'a
 
-    if 0.6 /* rand() is pseudo */ > 0.5 {   
+    if rand() /* rand() is pseudo. Put 0.6 or 0.4 to test. */ > 0.5 {   
         *x = 84;
     } else {
         println!("{}", r);    // 'a
     }
-    println!("{}", r);
+    // println!("{}", r); Activate this line and code will not compile.
 }
 
 
